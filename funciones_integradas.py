4688483bfaa06f481c5c3e003a47ed265a1d12e2
cruzalegui <cruzalegui.manager@gmail.com>
2021-12-21 12:09:56 -0500

    agregamos funciones_integradas.py

--- funciones_integradas.py
+++ funciones_integradas.py
@@ -1,10 +1,34 @@
 #FUNCIONES INTEGRADAS
 
-n = int("20")
-
+n = int("20") #convierte una cadena en un entero
 print(n)
 print(type(n))
 
-n = str(33)
+n = str(33) #convierte un entero en una cadena
 print(n)
 print(type(n))
+
+n = bin(20) #convierte un entero en un binario
+print(n)
+print(type(n))
+
+n = hex(20) #convierte un entero en un valor hexadecimal
+print(n)
+print(type(n))
+
+n = int("0b1010",2) #primero va el número binario y despues la base a la que se convierte
+print(n)
+print(type(n))
+
+n = int("0x14",16) #convertimos un hexadecimal en entero
+print(n)
+print(type(n))
+
+n = abs(-8) #genera el valor absoluto de un numero
+print(n)
+
+n = round(5.3) #redondea al número más cercano
+print(n)
+
+n = len("renato")  #cuenta el número de caracteres que tiene una cadena
+print(n)
