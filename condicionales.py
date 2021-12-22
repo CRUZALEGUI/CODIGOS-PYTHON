9475ad24938a27c30d387f096c166ad456c55d2f
cruzalegui <cruzalegui.manager@gmail.com>
2021-12-22 00:13:12 -0500

    agregamos condicionales

---
+++ condicionales.py
@@ -0,0 +1,10 @@
+#Condicionales
+
+numero = int(input('digite un numero: '))
+
+if numero>0:
+    print('el número es positivo')
+elif numero==0:
+    print('el numero es igual a cero')
+else:
+    print('el número es negativo')
